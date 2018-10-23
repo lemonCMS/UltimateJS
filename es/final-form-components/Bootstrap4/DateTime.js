f4f9d9f336f9ecc225f6c78aaf5229df5c7ec1c4
raymond <rsvvuuren@hotmail.com>
2018-10-23 11:18:46 +0200

    update final-form

--- es/final-form-components/Bootstrap4/DateTime.js
+++
@@ -1,7 +0,0 @@
-import Wrap from './Wrappers/Wrap';
-import DateTimeRender from './Types/DateTime';
-import decorator from '../utils/decorator';
-export default decorator({
-  type: 'text',
-  component: DateTimeRender
-})(Wrap);
